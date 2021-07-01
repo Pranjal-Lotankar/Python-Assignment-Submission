215a1c83962e53a51664ac3851a854c712c5a4d6
Pranjal-Lotankar <85273165+Pranjal-Lotankar@users.noreply.github.com>
2021-07-01 19:22:35 +0530

    Assignment Day 1 Submission

---
+++ Assignment1.py
@@ -0,0 +1,8 @@
+a=int(input("enter a"))
+b=int(input("enter b"))
+Sub=a+b
+Mul=a*b
+Div=a/b
+print("subtraction of a and b is ",Sub)
+print("multiplication of a and b is ",Mul)
+print("division of a and b is ",Div)
